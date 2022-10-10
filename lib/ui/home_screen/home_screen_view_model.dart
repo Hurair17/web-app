ea08b4f90c2285355662c440106d6e3913c1e148
unknown <hurairmohammad17@gmail.com>
2022-10-10 22:50:45 +0500

    new commit

--- lib/ui/home_screen/home_screen_view_model.dart
+++ lib/ui/home_screen/home_screen_view_model.dart
@@ -20,14 +20,5 @@ class HomeScreenViewModel extends ChangeNotifier {
       'details':
           'A typical South Indian midly apicy simbar rice meal A typical South Indian midly apicy simbar rice meal',
     },
-    '2': {
-      'title': 'Sambar Rice',
-      'price': 200,
-      'reviews': 230 / 300,
-      'img':
-          'https://www.vegrecipesofindia.com/wp-content/uploads/2021/05/sambar-1.jpg',
-      'details':
-          'A typical South Indian midly apicy simbar rice meal A typical South Indian midly apicy simbar rice meal',
-    }
   };
 }
